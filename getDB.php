c964e7685265e8b64c98f1b6ee1bdccd67df7395
Leo Adberg <ladberg@berkeley.edu>
2016-11-12 17:57:06 -0800

    updated JSON

--- getDB.php
+++ getDB.php
@@ -8,7 +8,7 @@ foreach ($posts as $post) {
 			<section class=\"box special\">
 			<span class=\"image featured\"><img src=\"pics/small/" . $post->img . ".JPG\" /></span>
 			<h3>" . $post->title . "</h3>
-			<p> . $post->desc . <p>
+			<p>" . $post->desc . "<p>
 			<ul class=\"actions\">
 			<li><a href=\"#\" class=\"button alt\">View Image</a></li>
 			</ul></section></div>
